066919465cd67a83a357da42cc902c03c69ebec8
LuisGilGB <luis.gilgb@gmail.com>
2025-04-02 23:54:00 +0200

    Moved demo assets to the public directory

--- app/server-functions/getContainerFiles.ts
+++ app/server-functions/getContainerFiles.ts
@@ -1,5 +1,5 @@
-import { files } from "@/demos/files";
 import { createServerFn } from "@tanstack/react-start"
+import { files } from "public/test-cases/files";
 
 // TODO: make this static when the bug is fixed: https://github.com/TanStack/router/issues/3823
 export const getContainerFiles = createServerFn(/*{ type: 'static' }*/).handler(async () => {
