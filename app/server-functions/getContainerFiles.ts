1090debc86764ca2dbbb5c2f974d84b92dea0ec2
LuisGilGB <luis.gilgb@gmail.com>
2025-04-03 00:09:31 +0200

    Trying to handle server function errors differently

--- app/server-functions/getContainerFiles.ts
+++ app/server-functions/getContainerFiles.ts
@@ -3,5 +3,10 @@ import { files } from "public/test-cases/files";
 
 // TODO: make this static when the bug is fixed: https://github.com/TanStack/router/issues/3823
 export const getContainerFiles = createServerFn(/*{ type: 'static' }*/).handler(async () => {
-  return files;
+  try {
+    return files;
+  } catch (error) {
+    console.error(error);
+    return { error: error as string };
+  }
 });
